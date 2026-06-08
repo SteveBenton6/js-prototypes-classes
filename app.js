1d741b5560c4f84e2b2b94e02fe60e1cfd737ae0
Steve Benton <steve@bentonemail.uk>
2026-06-08 11:37:09 +0100

    add html and js template

---
+++ app.js
@@ -0,0 +1,21 @@
+// Example 1 -  What on Earth are Prototypes
+console.log("\nSECTION 1 - What on Earth are Prototypes");
+// No Code
+
+// Example 2 -  An Intro to OOP
+console.log("\nSECTION 2 - An Intro to OOP");
+
+// Example 3 -  Factory Functions
+console.log("\nSECTION 3 - Factory Functions");
+
+// Example 4 -  Constructor Functions
+console.log("\nSECTION 4 - Constructor Functions");
+
+// Example 5 -  JS Classes - Syntactical Sugar
+console.log("\nSECTION 5 - JS Classes - Syntactical Sugar");
+
+// Example 6 -  A Bit More Practice with Classes
+console.log("\nSECTION 6 - A Bit More Practice with Classes");
+
+// Example 7 -  Extends, Super, and Subclasses
+console.log("\nSECTION 7 - Extends, Super, and Subclasses");
