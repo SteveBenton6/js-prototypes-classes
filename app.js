33cb94aec2292392e10c2078202b1a2f9142066e
Steve Benton <steve@bentonemail.uk>
2026-06-09 15:31:15 +0100

    add constructor functions

--- app.js
+++ app.js
@@ -38,6 +38,32 @@ console.log(firstColor.hex());
 // Example 4 -  Constructor Functions
 console.log("\nSECTION 4 - Constructor Functions");
 
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${g}, ${b})`;
+};
+Color.prototype.hex = function () {
+  const { r, g, b } = this;
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+Color.prototype.rgba = function (a = 1.0) {
+  const { r, g, b } = this;
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
+const color1 = new Color(255, 50, 0);
+console.log(color1);
+console.log(color1.rgb());
+console.log(color1.hex());
+console.log(color1.rgba(0.8));
+// document.body.style.backgroundColor = color1.rgb();
+// document.body.style.backgroundColor = color1.hex();
+document.body.style.backgroundColor = color1.rgba(0.6);
+
 // Example 5 -  JS Classes - Syntactical Sugar
 console.log("\nSECTION 5 - JS Classes - Syntactical Sugar");
 
