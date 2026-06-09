9ee86bc6a76612e87ba95bd0b4517eac779fa4f5
Steve Benton <steve@bentonemail.uk>
2026-06-09 17:11:39 +0100

    add more calss methods

--- app.js
+++ app.js
@@ -71,23 +71,82 @@ class Colour {
   constructor(r, g, b, name) {
     this.r = r;
     this.g = g;
-    this.b - b;
+    this.b = b;
     this.name = name;
+    this.calcHSL();
   }
   innerRGB() {
+    let { r, g, b } = this;
     return `(${r}, ${g}, ${b})`;
   }
   rgb() {
-    return `rgb(this.innerRGB()})`;
+    let { r, g, b } = this;
+    return `rgb(${this.innerRGB()})`;
   }
   hex() {
+    let { r, g, b } = this;
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
   rgba(a = 1.0) {
     return `rgba(${this.innerRGB()}, ${a})`;
   }
+  hsl() {
+    let { h, s, l } = this;
+    return `hsl(${h},${s}%, ${l}%)`;
+  }
+  fulllySaturated() {
+    let { h, l } = this;
+    return `hsl(${h},100%, ${l}%)`;
+  }
+  opposite() {
+    let { h, s, l } = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue},${s}%, ${l}%)`;
+  }
+  calcHSL() {
+    let { r, g, b } = this;
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r)
+      // Red is max
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      // Green is max
+      h = (b - r) / delta + 2;
+    else
+      // Blue is max
+      h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0) h += 360;
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
 }
-const c1 = new Color(255, 67, 89, "tomato");
+const c1 = new Colour(255, 67, 89, "tomato");
 console.log(c1);
 console.log(c1.rgb());
 console.log(c1.hex());
@@ -95,6 +154,9 @@ console.log(c1.rgba(0.7));
 
 // Example 6 -  A Bit More Practice with Classes
 console.log("\nSECTION 6 - A Bit More Practice with Classes");
+
+console.log(c1.hsl());
+document.body.style.backgroundColor = c1.opposite();
 
 // Example 7 -  Extends, Super, and Subclasses
 console.log("\nSECTION 7 - Extends, Super, and Subclasses");
