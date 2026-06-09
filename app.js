c7833b7a0c18c4b5b736c5168610fd8010a35ac1
Steve Benton <steve@bentonemail.uk>
2026-06-09 12:40:19 +0100

    add factory functions

--- app.js
+++ app.js
@@ -16,6 +16,25 @@ console.log("\nSECTION 2 - An Intro to OOP");
 // Example 3 -  Factory Functions
 console.log("\nSECTION 3 - Factory Functions");
 
+function makeColor(r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+  color.hex = function () {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  };
+  return color;
+}
+const firstColor = makeColor(35, 255, 150);
+console.log(firstColor.rgb());
+console.log(firstColor.hex());
+
 // Example 4 -  Constructor Functions
 console.log("\nSECTION 4 - Constructor Functions");
 
